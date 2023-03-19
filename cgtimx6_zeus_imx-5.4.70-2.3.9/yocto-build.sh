863b71b0ff0d201b0961603de815da13e1d2edd8
Tan Le <lmtan91@gmail.com>
2023-03-19 23:23:07 +0700

    integrate swupdate
    - add patches
    - enable swupdate

--- cgtimx6_zeus_imx-5.4.70-2.3.9/yocto-build.sh
+++ cgtimx6_zeus_imx-5.4.70-2.3.9/yocto-build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # This script will run into container
 
+################################################################################
+#################################### Function ##################################
+################################################################################
+apply_patch() {
+	for file in ${DOCKER_WORKDIR}/${ARISTA_IMX_RELEASE}/patches/*
+	do
+		patch -p0 -N --dry-run --silent < $file 2>/dev/null
+		if [ $? -eq 0 ];
+		then
+    		echo "Applying patches $file ..."
+    		patch -p0 -N < $file
+		fi
+	done
+}
+
 # source the common variables
 
 . cgtimx6_zeus_imx-5.4.70-2.3.9/env.sh
@@ -19,6 +34,8 @@ repo init \
 
 repo sync -j`nproc`
 
+apply_patch ;
+
 # source the yocto env
 
 EULA=1 MACHINE="${MACHINE}" DISTRO="${DISTRO}" source imx-setup-release.sh -b build_${DISTRO}
@@ -34,6 +51,7 @@ if [ -d ../sources/meta-java ]; then
     echo "BBLAYERS += \"\${BSPDIR}/sources/meta-java\"" >> ./conf/bblayers.conf
 fi
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-webserver\"" >> ./conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-swupdate\"" >> ./conf/bblayers.conf
 
 echo "# Possible provider: cacao-initial-native and jamvm-initial-native" >> ./conf/local.conf
 echo "PREFERRED_PROVIDER_virtual/java-initial-native = \"cacao-initial-native\"" >> ./conf/local.conf
@@ -46,6 +64,9 @@ echo "PREFERRED_PROVIDER_java2-runtime = \"openjdk-8-jre\"" >> ./conf/local.conf
 echo "CORE_IMAGE_EXTRA_INSTALL += \"openssh\"" >> ./conf/local.conf
 echo "PACKAGE_EXCLUDE += \" packagegroup-core-ssh-dropbear\"" >> ./conf/local.conf
 echo "CORE_IMAGE_EXTRA_INSTALL += \"packagegroup-core-ssh-openssh\"" >> ./conf/local.conf
+echo "IMAGE_FSTYPES = \" ext4 ext4.gz wic.bmap wic.gz\"" >> ./conf/local.conf
+
+echo "PREFERRED_PROVIDER_u-boot-fw-utils = \"libubootenv\"" >> ./conf/local.conf
 
 # Build
 
@@ -58,6 +79,5 @@ fi
 echo "INHERIT += \"extrausers\"" >> ./conf/local.conf
 echo "EXTRA_USERS_PARAMS = \"usermod -P arista root;\"" >> ./conf/local.conf
 
-bitbake pseudo
 bitbake ${IMAGES}
 
